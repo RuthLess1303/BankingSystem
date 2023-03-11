f09b32035a1338808318a95d9ab938905c19ea43
ruthless <jdundua@gmail.com>
2023-03-11 21:00:57 +0400

    migration deleted from internetBankCore and added in BankingSystemSharedDb

--- InternetBankCore/Program.cs
+++ InternetBankCore/Program.cs
@@ -1,7 +1,5 @@
-﻿using InternetBankCore.Db;
-using InternetBankCore.Services;
+﻿using InternetBankCore.Services;
 using InternetBankCore.Validations;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using var host = Host.CreateDefaultBuilder(args)
@@ -15,6 +13,3 @@ using var host = Host.CreateDefaultBuilder(args)
         services.AddTransient<IUserService, UserService>();
     })
     .Build();
-
-var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
-optionsBuilder.UseSqlServer("Server=localhost;Database=MobileBankDb;User Id=sa; Password=wavedi123;Encrypt=False;");
