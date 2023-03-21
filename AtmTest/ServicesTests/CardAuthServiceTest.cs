b558acd3326c6e166cbf0c61baf25f9fbeadded0
Nika <nika.nacvlishvili1@gmail.com>
2023-03-21 21:08:51 +0400

    ATM Unit Tests

---
+++ AtmTest/ServicesTests/CardAuthServiceTest.cs
@@ -0,0 +1,119 @@
+﻿using AtmCore.Repositories;
+using AtmCore.Requests;
+using AtmCore.Services;
+using AtmCore.Validations;
+using BankingSystemSharedDb.Db;
+using BankingSystemSharedDb.Db.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AtmTest.ServicesTests;
+
+[TestFixture]
+public class CardAuthServiceTests
+{
+    private AppDbContext _context;
+    private IAccountRepository _accountRepository;
+    private ICardRepository _cardRepository;
+    private WithdrawalRequestValidation _requestValidation;
+    
+    [SetUp]
+    public void Setup()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase("TestDatabase")
+            .Options;
+
+        _context = new AppDbContext(options);
+        _context.Database.EnsureCreated();
+
+        _accountRepository = new AccountRepository(_context);
+        _cardRepository = new CardRepository(_context);
+        _requestValidation = new WithdrawalRequestValidation();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        _context.Database.EnsureDeleted();
+        _context.Dispose();
+    }
+
+    [Test]
+    public async Task GetAuthorizedAccountAsync_InvalidPin_ThrowsArgumentException()
+    {
+        // Arrange
+        _context.Account.Add(new AccountEntity
+        {
+            Id = Guid.NewGuid(),
+            PrivateNumber = "1234567890",
+            Iban = "GB12TEST3456789012",
+            CurrencyCode = "GBP",
+            Balance = 1000,
+            CreationDate = DateTime.UtcNow
+        });
+
+        _context.CardAccountConnection.Add(new CardAccountConnectionEntity
+        {
+            Id = 1,
+            CardId = Guid.NewGuid(),
+            Iban = "GB12TEST3456789012",
+            CreationDate = DateTime.UtcNow
+        });
+
+        await _context.SaveChangesAsync();
+
+        var sut = new CardAuthService(
+            new AccountRepository(_context),
+            new CardRepository(_context),
+            new WithdrawalRequestValidation() // Use WithdrawalRequestValidator instead of WithdrawalRequestValidation
+        );
+
+        // Act & Assert
+        Assert.ThrowsAsync<ArgumentException>(() => sut.GetAuthorizedAccountAsync(new WithdrawalRequest
+        {
+            CardNumber = "1234567890123456",
+            PinCode = "1234",
+            Amount = 100
+        }));
+    }
+
+
+    [Test]
+    public Task GetAuthorizedAccountAsync_InvalidCardNumber_ThrowsArgumentException()
+    {
+        // Arrange
+        var request = new WithdrawalRequest
+        {
+            CardNumber = "123456789012345", // 15 digits instead of 16
+            PinCode = "1234",
+            Amount = 100
+        };
+        _requestValidation.ValidateCreditCardNumber(request.CardNumber);
+
+        var sut = new CardAuthService(_accountRepository, _cardRepository, _requestValidation);
+
+        // Act & Assert
+        Assert.ThrowsAsync<ArgumentException>(async () => await sut.GetAuthorizedAccountAsync(request));
+        return Task.CompletedTask;
+    }
+
+    [Test]
+    public Task GetAuthorizedAccountAsync_CardDoesNotExist_ThrowsArgumentException()
+    {
+        // Arrange
+        var request = new WithdrawalRequest
+        {
+            CardNumber = "1234567890123456",
+            PinCode = "1234",
+            Amount = 100
+        };
+        _requestValidation.ValidatePinCode(request.PinCode);
+        _requestValidation.ValidateCreditCardNumber(request.CardNumber);
+
+        var sut = new CardAuthService(_accountRepository, _cardRepository, _requestValidation);
+
+        // Act & Assert
+        Assert.ThrowsAsync<ArgumentException>(async () => await sut.GetAuthorizedAccountAsync(request));
+        return Task.CompletedTask;
+    }
+}
---
+++ AtmTest/WithdrawalRequestValidationTest.cs
@@ -0,0 +1,55 @@
+using AtmCore.Validations;
+
+namespace AtmTest;
+
+[TestFixture]
+public class WithdrawalRequestValidationTests
+{
+    [SetUp]
+    public void SetUp()
+    {
+        _withdrawalValidation = new WithdrawalRequestValidation();
+    }
+
+    private WithdrawalRequestValidation _withdrawalValidation;
+
+    [TestCase("1234567890123456")]
+    [TestCase("12341234123412341")]
+    [TestCase("123412341234")]
+    [TestCase("4111111111111112")]
+    [TestCase("123456789012345")]
+    [TestCase("abcd123456789012")]
+    [TestCase("5559955555554444")]
+    [TestCase("123456")]
+    [TestCase("4111 1111 1111 111a")]
+    [TestCase("")]
+    [TestCase(null)]
+    public void ValidateCreditCardNumber_InvalidInput_ReturnsFalse(string input)
+    {
+        // Act
+        var result = _withdrawalValidation.ValidateCreditCardNumber(input);
+
+        // Assert
+        Assert.That(result, Is.False);
+    }
+
+    [TestCase("4111111111111111")]
+    [TestCase("5105105105105100")]
+    [TestCase("378282246310005")]
+    [TestCase("6011000990139424")]
+    [TestCase("30569309025904")]
+    [TestCase("3530111333300000")]
+    [TestCase("5555555555554444")]
+    [TestCase("4111111111111111")]
+    [TestCase("4012888888881881")]
+    [TestCase("4222222222222")]
+    [TestCase("5555555555554444")]
+    public void ValidateCreditCardNumber_ValidInput_ReturnsTrue(string input)
+    {
+        // Act
+        var result = _withdrawalValidation.ValidateCreditCardNumber(input);
+
+        // Assert
+        Assert.That(result, Is.True);
+    }
+}
