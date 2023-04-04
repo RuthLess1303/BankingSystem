0508a5db01262026d66360c9b7e572aa2d004c8a
ruthless <jdundua@gmail.com>
2023-04-04 11:58:05 +0400

    withdrawal request changed from decimal to int (amount)

--- BankingSystem.Atm.Core/Requests/WithdrawalRequest.cs
+++ BankingSystem.Atm.Core/Requests/WithdrawalRequest.cs
@@ -4,5 +4,5 @@ public class WithdrawalRequest
 {
     public string CardNumber { get; set; }
     public string PinCode { get; set; }
-    public decimal Amount { get; set; }
+    public int Amount { get; set; }
 }
